3b1823c96d5095c2ce497d46d003ab70be20e963
subakutty <42169909+subakutty@users.noreply.github.com>
2019-02-21 10:32:43 +0530

    Update vowels.c

--- vowels.c
+++ vowels.c
@@ -2,20 +2,20 @@
 int main()
 {
     char ch;
-    printf("Enter any character: ");
+    //printf("Enter any character: ");
     scanf("%c", &ch);
     if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || 
        ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
     {
-        printf("'%c' is Vowel.", ch);
+        printf("Vowel");
     }
     else if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
     {
-        printf("'%c' is Consonant.", ch);
+        printf("Consonant");
     }
     else 
     {
-        printf("'%c' is not an alphabet.", ch);
+        printf("invalid");
     }
 
     return 0;
